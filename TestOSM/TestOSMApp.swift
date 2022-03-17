b4e4a4ba1faf709764845e45fa5e4c9bc0adb16b
danyaqq <fedak220@gmail.com>
2022-03-17 11:10:18 +0300

    Initial Commit

---
+++ TestOSM/TestOSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOSMApp.swift
+//  TestOSM
+//
+//  Created by Даня on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestOSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
